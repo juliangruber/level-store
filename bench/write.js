15957b6f5ecf187cfca67bcaeaf82d4960dabbc7
Julian Gruber <julian@juliangruber.com>
2013-07-03 09:14:43 +0200

    add benchmark suite

---
+++ bench/write.js
@@ -0,0 +1,24 @@
+var ben = require('ben');
+var Store = require('..');
+var level = require('level');
+var fs = require('fs');
+var rimraf = require('rimraf');
+
+// ~1.35ms
+
+rimraf.sync(__dirname + '/.db');
+level(__dirname + '/.db', { valueEncoding: 'binary' }, function (err, db) {
+  if (err) throw err;
+
+  var store = Store(db);
+
+  var test = function (done) {
+    fs.createReadStream(__dirname + '/cat.jpg')
+      .pipe(store.createWriteStream(Math.random()+''))
+      .on('close', done);
+  };
+
+  ben.async(1000, test, function (ms) {
+    console.log('%s milliseconds per iteration', ms);
+  });
+});
